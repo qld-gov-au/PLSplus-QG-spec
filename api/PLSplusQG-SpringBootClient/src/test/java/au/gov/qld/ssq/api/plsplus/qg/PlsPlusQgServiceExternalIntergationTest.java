ec62e8051e8f95c1f748d7d9588e02f1261dda2a
william dutton <will.dutt@gmail.com>
2021-05-04 10:18:53 +1000

    finish junit4 to 5 upgrade where tags now work

--- api/PLSplusQG-SpringBootClient/src/test/java/au/gov/qld/ssq/api/plsplus/qg/PlsPlusQgServiceExternalIntergationTest.java
+++ api/PLSplusQG-SpringBootClient/src/test/java/au/gov/qld/ssq/api/plsplus/qg/PlsPlusQgServiceExternalIntergationTest.java
@@ -2,15 +2,13 @@ package au.gov.qld.ssq.api.plsplus.qg;
 
 import au.gov.qld.ssq.api.plsplus.qg.handler.ApiException;
 import au.gov.qld.ssq.api.plsplus.qg.model.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -24,7 +22,6 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {PlsPlusQgConfig.class, PlsPlusQgService.class},
     initializers = ConfigDataApplicationContextInitializer.class)
 public class PlsPlusQgServiceExternalIntergationTest {
--- api/PLSplusQG-SpringBootClient/src/test/java/au/gov/qld/ssq/api/plsplus/qg/PlsPlusQgServiceTest.java
+++ api/PLSplusQG-SpringBootClient/src/test/java/au/gov/qld/ssq/api/plsplus/qg/PlsPlusQgServiceTest.java
@@ -1,17 +1,14 @@
 package au.gov.qld.ssq.api.plsplus.qg;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Tag;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {PlsPlusQgConfig.class, PlsPlusQgService.class},
     initializers = ConfigDataApplicationContextInitializer.class)
 public class PlsPlusQgServiceTest {
